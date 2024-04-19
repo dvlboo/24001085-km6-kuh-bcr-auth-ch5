e8aa7e5bdd95031032505374724023d2b002f235
dvlboo <kuhhhckr@gmail.com>
2024-04-19 21:04:15 +0700

    fixed routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const auth = require('./auth')
 const superadmin = require('./superadmin')
+const sizes = require('./sizes')
+const cars = require('./cars')
 
-router.use("/auth", auth)
-router.use("/superadmin", superadmin)
+router.use('/auth', auth)
+router.use('/superadmin', superadmin)
+router.use('/sizes', sizes)
+router.use('/cars', cars)
 
 module.exports = router;
